fe71f5d8a58b28448e00be22958f70d1f8fe310e
hushen.beg <hushen.beg@stackaero.com>
2020-10-16 21:58:49 +0530

    orm crud operations

---
+++ orm_client.py
@@ -0,0 +1,238 @@
+import odoorpc
+
+class OdooClient:
+
+    def __init__(self, config):
+
+        self.odoo = odoorpc.ODOO(config['url'], port=config['port'])
+
+        self.config = config
+    
+    def login(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """        
+
+        try:
+            print('---------login initiated--------')
+
+            response = self.odoo.login(
+                self.config['db'], 
+                self.config['user'], 
+                self.config['password']
+            )
+
+            print('----------Login Successful--------')
+
+            # return response 
+
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+    
+    def get_dbs(self):
+        """[summary]
+
+        Returns:
+            [type]: [description]
+        """        
+        try:
+
+            db_list = self.odoo.db.list()
+
+            return db_list
+        
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+    
+    def create(self, body):
+        """[summary]
+
+        Args:
+            body ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """        
+
+        try:
+            action = None
+
+            if 'create' in body['action']:
+
+                action = 'create'
+
+            user = self.odoo.env.user
+
+            print('---------model----------', body['model'])
+
+            print('--------action------------', action)
+
+            print('------------user_id-----------', [user.id])
+
+            read_user_data = self.odoo.env['hr.employee'].create(
+                { 
+                    "create_uid": 4, 
+                    "id": 11, 
+                    "name": 'Ajay' 
+                }
+            )
+
+            return read_user_data
+        
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+    
+    def read(self, body):
+        """[summary]
+
+        Args:
+            body ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """        
+
+        try:
+            action = None
+
+            print('------------body--------', body)
+
+            if 'read' in body['action']:
+
+                action = 'read'
+
+            user = self.odoo.env.user
+
+            print('---------model----------', body['model'])
+
+            print('--------action------------', action)
+
+            print('------------user_id-----------', [user.id])
+
+            read_user_data = self.odoo.execute(body['model'], action, [user.id])
+
+            return read_user_data
+        
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+    
+    def update(self, body):
+        """[summary]
+
+        Args:
+            body ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """        
+
+        try:
+            action = None
+
+            if 'action' in body and body['action'] == 'write':
+
+                action = 'write'
+
+            user = self.odoo.env.user
+
+            update_user_data = self.odoo.env['hr.employee'].write(
+                [user.id], {'name': "MyContact"}
+            )
+
+            return update_user_data
+        
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+    
+    def delete(self, body):
+        """[summary]
+
+        Args:
+            body ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """        
+
+        try:
+            action = None
+
+            if 'action' in body and body['action'] == 'unlink':
+
+                action = 'unlink'
+
+            user = self.odoo.env.user
+
+            # read_user_data = self.odoo.execute(body['model'], action, [user.id])
+
+            # return read_user_data
+
+            delete_user_data = self.odoo.env['hr.employee'].unlink(
+                [user.id]
+            )
+
+            return delete_user_data
+        
+        except Exception as error:
+
+            return {
+                'status_code':400,
+                'status_message':'got an exception {}'.format(error)
+            }
+
+
+
+
+
+# # Prepare the connection to the server
+# odoo = odoorpc.ODOO('localhost', port=8069)
+
+# # Check available databases
+# print(odoo.db.list())
+
+# # Login
+# odoo.login('employee', 'hushen.beg@gmail.com', 'Doche@786')
+
+# # Current user
+# user = odoo.env.user
+# print(user.name)            # name of the user connected
+# print(user.company_id.name) # the name of its company
+
+# # Simple 'raw' query
+# user_data = odoo.execute('res.users', 'read', [user.id])
+
+# print('-------user_data-------', user_data)
+
+# # Use all methods of a model
+# if 'sale.order' in odoo.env:
+#     Order = odoo.env['sale.order']
+#     order_ids = Order.search([])
+#     for order in Order.browse(order_ids):
+#         print(order.name)
+#         products = [line.product_id.name for line in order.order_line]
+#         print(products)
+
+# # Update data through a record
+# user.name = "Brian Jones"
---
+++ orm_client_test.py
@@ -0,0 +1,38 @@
+from orm_client import OdooClient
+
+config  = {
+    "url":"localhost",
+    "db":"employee",
+    "user":"xxxxx@gmail.com",
+    "password":"xxxxx",
+    "port":8069
+}
+
+body = {
+    'action': ('create','read', 'write', 'search', 'unlink'),
+    'model':'hr.employee'
+    # 'model':'res.users'
+}
+
+
+client = OdooClient(config)
+
+client.login()
+
+# print('----------login_response------', login_response)
+
+# create_response = client.create(body)
+
+# print('---------create_response-----------', create_response)
+
+# read_response = client.read(body)
+
+# print('----------read_response---------', read_response)
+
+# update_response = client.update(body)
+
+# print('-----------update_response----------', update_response)
+
+delete_response = client.delete(body)
+
+print('-----------delete_response-----------', delete_response)
