2dc6a3b573deb9db44f41a9fb12fe0d993e104b0
Hushen Beg <hushen.beg@gmail.com>
2020-10-19 15:39:48 +0530

    Update orm_client.py
    removed the commented code

--- orm_client.py
+++ orm_client.py
@@ -201,38 +201,4 @@ class OdooClient:
                 'status_code':400,
                 'status_message':'got an exception {}'.format(error)
             }
-
-
-
-
-
-# # Prepare the connection to the server
-# odoo = odoorpc.ODOO('localhost', port=8069)
-
-# # Check available databases
-# print(odoo.db.list())
-
-# # Login
-# odoo.login('employee', 'hushen.beg@gmail.com', 'Doche@786')
-
-# # Current user
-# user = odoo.env.user
-# print(user.name)            # name of the user connected
-# print(user.company_id.name) # the name of its company
-
-# # Simple 'raw' query
-# user_data = odoo.execute('res.users', 'read', [user.id])
-
-# print('-------user_data-------', user_data)
-
-# # Use all methods of a model
-# if 'sale.order' in odoo.env:
-#     Order = odoo.env['sale.order']
-#     order_ids = Order.search([])
-#     for order in Order.browse(order_ids):
-#         print(order.name)
-#         products = [line.product_id.name for line in order.order_line]
-#         print(products)
-
-# # Update data through a record
-# user.name = "Brian Jones"
+        
