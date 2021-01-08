6739b3721855732679095e3f6c4b2556a4ad08b4
Hushen Beg <hushen.beg@gmail.com>
2021-01-08 20:58:29 +0530

    Update orm_client_test.py
    updated with the object

--- orm_client_test.py
+++ orm_client_test.py
@@ -21,13 +21,23 @@ client.login()
 
 # print('----------login_response------', login_response)
 
-# create_response = client.create(body)
+create_object = { 
+    "create_uid": 9, 
+    "id": 45, 
+    "name": 'Rohit Sharma' 
+}
 
-# print('---------create_response-----------', create_response)
+create_response = client.create(create_object)
+
+print('---------create_response-----------', create_response)
 
 # read_response = client.read(body)
 
 # print('----------read_response---------', read_response)
+
+update_object = {
+    'name': "Irfan Pathan"
+}
 
 # update_response = client.update(body)
 
